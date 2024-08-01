9d74fc4b5fc57483f8ba9798151f4b8bfb34340d
Gabriel Rodrigues <gabriel.r_00@hotmail.com>
2024-08-01 18:00:37 -0300

    configurando sequelize

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const dbConfig = require('../config/db');
+const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use(routes)
+
+
 
 module.exports = app;
---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get("/", async (req, res) => {
+  res.send("Página iniciada localmente!")
+})
+
+module.exports = routes;
