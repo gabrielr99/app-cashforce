fde3006dade8bd04a52a636f85d21943f5f4e2b3
Gabriel Rodrigues <gabriel.r_00@hotmail.com>
2024-08-03 15:14:33 -0300

    criando migration e model sponsors

---
+++ backend/src/database/models/sponsorsModel.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Sponsors extends Model {
+  static init(sequelize) {
+    super.init({
+      name: DataTypes.STRING,
+      tradingName: DataTypes.STRING,
+      cashforceTax: DataTypes.STRING,
+      responsibleName: DataTypes.STRING,
+      responsibleEmail: DataTypes.STRING,
+      responsiblePosition: DataTypes.STRING,
+      responsiblePhone: DataTypes.STRING,
+      responsibleMobile: DataTypes.STRING,
+      website: DataTypes.STRING,
+      postalCode: DataTypes.STRING,
+      address: DataTypes.STRING,
+      number: DataTypes.STRING,
+      complement: DataTypes.STRING,
+      neighborhood: DataTypes.STRING,
+      city: DataTypes.STRING,
+      state: DataTypes.STRING,
+      bank: DataTypes.STRING,
+      bankAgency: DataTypes.STRING,
+      account: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
+      situation: DataTypes.STRING,
+      situationDate: DataTypes.STRING,
+      cnpjId: DataTypes.INTEGER,
+      email: DataTypes.STRING,
+    }, {
+      sequelize
+    })
+  }
+}
+
+module.exports = Sponsors;
